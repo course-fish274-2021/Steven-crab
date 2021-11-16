e5233593621666d45466a7e919eeff7ba528a6ca
sr320 <roberts.sbr@gmail.com>
2021-11-16 10:15:41 -0800

    remove fish with scale lenght < 1

--- code/fish-analysis.R
+++ code/fish-analysis.R
@@ -9,7 +9,12 @@ library(dplyr)
 # adding column to denote fish is big or small using 300 as cutoff
 
 fish_data_cat <- fish_data %>%
+  filter(scalelength > 1) %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+
+
+
 
 
 
