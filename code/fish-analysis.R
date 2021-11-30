812c24a5d7e9b3491820209d5c81f2e39c67a284
sr320 <roberts.sbr@gmail.com>
2021-11-30 09:52:25 -0800

    making rmd file

--- code/fish-analysis.R
+++ code/fish-analysis.R
@@ -12,6 +12,9 @@ fish_data_cat <- fish_data %>%
   filter(scalelength > 1) %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
+
+library(ggplot2)
+
 # Plot historgram of fish data
 ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
   geom_histogram()
